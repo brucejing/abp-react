21d39f982ac540fce947260c3c5133c09822f782
Bruce <brucejing@hotmail.com>
2024-11-23 11:51:20 -0500

    Restored DB. Solved 'Microsoft.EntityFrameworkCore.Migrations.PendingModelChangesWarning' error that is caused by missing migration for pending model changes.

--- aspnet-core/src/Abp.React.EntityFrameworkCore/Migrations/20241123164714_Update_AbpAuditLogs.Designer.cs
+++ aspnet-core/src/Abp.React.EntityFrameworkCore/Migrations/20241123164714_Update_AbpAuditLogs.Designer.cs
@@ -4,6 +4,7 @@ using Abp.React.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Abp.React.Migrations
 {
     [DbContext(typeof(ReactDbContext))]
-    partial class ReactDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241123164714_Update_AbpAuditLogs")]
+    partial class Update_AbpAuditLogs
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
